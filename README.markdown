9a3e23eff42d35403f4b26821bea0523efb11695
Firmansyah Nuralif Rohman <mendoanjoe@gmail.com>
2019-09-13 16:38:19 +0700

    frist commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# Puisi
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/mendoanjoe/pen/GRKGQzB](https://codepen.io/mendoanjoe/pen/GRKGQzB).
+
+ 
---
+++ script.js
@@ -0,0 +1,6 @@
+var typed6 = new Typed('#typed', {
+    strings: ['<h2><b>Aku</b></h2>Kalau sampai waktuku<br>Aku mau tak seorang kan merayu<br>Tidak juga kau<br>Tak perlu sedan itu<br> <br>Aku ini binatang jalang<br>Dari kumpulannya terbuang<br>Biar peluru menembus kulitku<br>Aku tetap meradang menerjang<br> <br>Luka dan bisa kubawa berlari<br>Berlari<br> Hingga hilang pedih peri<br>Dan aku akan lebih tidak peduli<br>Aku mau hidup seribu tahun lagi<br>-<i> Chairil Anwar<i>'],
+    typeSpeed: 100,
+    backSpeed: 0,
+    loop: false
+  });

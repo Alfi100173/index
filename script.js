539e5992e3a8f0cb36b4437fa4072aeb907fb3da
Firmansyah Nuralif Rohman <mendoanjoe@gmail.com>
2019-09-15 19:30:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const puisi1 = '<h2><b>Aku</b></h2>Kalau sampai waktuku<br>Aku mau tak seorang k
 const puisi2 = '<h2><b>Hidup</b></h2>Ini tentang hidup<br>Dimana hidup dengan sebuah cita mustahil,<br>Dengan segala mimpi jauh di angan<br>Hebat bukan,<br> <br>Padahal tidak, sama sekali tidak<br>Jiwa ini sudah lelah<br>Banyak tanya, bimbang dan resah menyatu<br>Hampir semuanya belum terjawab, Entah kapan<br><br>Terimakasih aku, sudah menemani aku<br>Dari awal sampai akhirku<br>-<i> Firmansyah Nuralif Rohman<i>'
 const typed6 = new Typed('#typed', {
     strings: [puisi1, '<br><br>', puisi2],
-    typeSpeed: 100,
+    typeSpeed: 300,
     backSpeed: 0,
     loop: false
   });

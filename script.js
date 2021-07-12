e265038916d7960bb85fea36898c75879077fbd2
Alfi100173 <87294999+Alfi100173@users.noreply.github.com>
2021-07-12 12:18:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const puisi1 = '<h2><b>Aku</b></h2>Kalau sampai waktuku<br>Aku mau tak seorang kan merayu<br>Tidak juga kau<br>Tak perlu sedan itu<br> <br>Aku ini binatang jalang<br>Dari kumpulannya terbuang<br>Biar peluru menembus kulitku<br>Aku tetap meradang menerjang<br> <br>Luka dan bisa kubawa berlari<br>Berlari<br> Hingga hilang pedih peri<br>Dan aku akan lebih tidak peduli<br>Aku mau hidup seribu tahun lagi<br>-<i> Chairil Anwar<i>';
-const puisi2 = '<h2><b>Hidup</b></h2>Ini tentang hidup<br>Dimana hidup dengan sebuah cita mustahil,<br>Dengan segala mimpi jauh di angan<br>Hebat bukan,<br> <br>Padahal tidak, sama sekali tidak<br>Jiwa ini sudah lelah<br>Banyak tanya, bimbang dan resah menyatu<br>Hampir semuanya belum terjawab, Entah kapan<br><br>Terimakasih aku, sudah menemani aku<br>Dari awal sampai akhirku<br>-<i> Firmansyah Nuralif Rohman<i>'
+const puisi1 = '<h2><b>Aku Ingin </b></h2>Aku Ingin Mencintaimu Dengan Sederhana<br>Dengan Kata Yang Tak Sempat Diucapkan<br>Kayu Terhadap Api Yang Menjadikannya Abu<br> <br>Aku Ingin Mencintaimu Dengan Sederhana<br>Dengan Isyarat Yang Tak Sempat Disampaikan<br>Awan Kepada Hujan Yang Menjadikannya Tiada<br>-<i> Sapardi Djoko<i>';
+const puisi2 = '<h2><b>HI</b></h2>Sudah berapa lama ya kita tak jumpa?<br>Terimakasih tuan putri sudah menjadi pelangi dalam hidupku :)<br>Maaf aku belum bisa jadi yang terbaik buat tuan putri :(<br>Oke sudah ya mengsedihnya...<br> <br>Selamat Ulang Tahun Tuan Putri<br>Tidak terasa ya umurmu kini sudah menginjak 22 tahun<br>Usia yang tidak muda lagi ya hehehe...<br>Mau ngomong apa lagi ya heheheheh aku kehabisan kata-kata nih hehehehe <br>Nanti di tonton ya videonya<br> <br>Selamat Ulang tahun <br>Untuk wanita terindahku yang mampu membuat hidupku <br>terasa sempurna<br>-<i> Muhammad Alfi Syahrin<i>'
 const typed6 = new Typed('#typed', {
-    strings: [puisi1, puisi2],
-    typeSpeed: 300,
+    strings: [puisi1, puisi2,],
+    typeSpeed: 100,
     backSpeed: 0,
     loop: false
   });
